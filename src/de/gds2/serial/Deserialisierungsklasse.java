c8eed87e406c48dc4788121ed2b168eb1f241a2b
phantomenias <95676595+phantomenias@users.noreply.github.com>
2022-06-01 11:03:14 +0200

    Serialisierung Aufgabe 1 - 1.6.22 - Mit Deserialisierung

---
+++ src/de/gds2/serial/Deserialisierungsklasse.java
@@ -0,0 +1,22 @@
+package de.gds2.serial;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+public class Deserialisierungsklasse {
+    public static void main(String[] args) {
+        Employee e = null;
+        try {
+            FileInputStream fileIn = new FileInputStream("C:\\Users\\phant\\Desktop\\Escuela\\Prog\\Test\\src\\de\\gds2\\serial\\employee.txt");
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            e = (Employee) in.readObject();
+            in.close();
+            fileIn.close();
+        } catch (ClassNotFoundException | IOException e1) {
+            e1.printStackTrace();
+        }
+        System.out.println(e.getName() + " " + e.getVorname());
+    }
+}
